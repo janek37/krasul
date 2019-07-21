bcf0fdaef244048aaeb65fe46b732995f13ce6f9
Jan Szejko <janek37@gmail.com>
2019-07-21 21:20:03 +0200

    Add base crossword models

--- krasul/crosswords/models.py
+++ krasul/crosswords/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Crossword(models.Model):
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+
+class Square(models.Model):
+    crossword = models.ForeignKey(Crossword, on_delete=models.CASCADE)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    solution = models.CharField(max_length=4, blank=True)
+
+    class Meta:
+        ordering = ['y', 'x']
+
+
+class Entry(models.Model):
+    squares = models.ManyToManyField(Square, through='EntrySquare')
+    clue = models.TextField()
+
+
+class EntrySquare(models.Model):
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    square = models.ForeignKey(Square, on_delete=models.CASCADE)
+    index = models.IntegerField()
